b6c36a0c34441d75736245f8ba7c20307172e486
ItzLightyHD <itzlightyhd@gmail.com>
2021-06-07 14:35:35 +0200

    Adios Dotenv

--- src/main/java/org/apexie/eliteclient/Config.java
+++ src/main/java/org/apexie/eliteclient/Config.java
@@ -1,10 +1,6 @@
 package org.apexie.eliteclient;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Config {
-
-    private static final Dotenv dotenv = Dotenv.load();
 
     public static String get(String key) {
         return System.getenv().get(key.toUpperCase());
